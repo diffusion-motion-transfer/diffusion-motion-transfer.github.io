bbe4b744aee74fa64ab6b029431fba52fd092467
Rafail Fridman <RafailFridman@gmail.com>
2023-11-23 17:52:08 +0200

    adding carousel

--- static/js/index.js
+++ static/js/index.js
@@ -1,21 +1,83 @@
-window.HELP_IMPROVE_VIDEOJS = false;
+//window.HELP_IMPROVE_VIDEOJS = false;
+//
+//
+//$(document).ready(function() {
+//    // Check for click events on the navbar burger icon
+//
+//    var options = {
+//			slidesToScroll: 1,
+//			slidesToShow: 1,
+//			loop: true,
+//			infinite: true,
+//			autoplay: true,
+//			autoplaySpeed: 5000,
+//    }
+//
+//		// Initialize all div with carousel class
+//    var carousels = bulmaCarousel.attach('.carousel', options);
+//
+//    bulmaSlider.attach();
+//
+//})
 
 
-$(document).ready(function() {
-    // Check for click events on the navbar burger icon
+// bootstrap 4 breakpoints
+const breakpoint = {
+  // small screen / phone
+  sm: 576,
+  // medium screen / tablet
+  md: 768,
+  // large screen / desktop
+  lg: 992,
+  // extra large screen / wide desktop
+  xl: 1200
+};
 
-    var options = {
-			slidesToScroll: 1,
-			slidesToShow: 1,
-			loop: true,
-			infinite: true,
-			autoplay: true,
-			autoplaySpeed: 5000,
+// bootstrap 4 responsive multi column slick carousel
+$(document).ready(function(){
+$('#slick').slick({
+  autoplay: true,
+  autoplaySpeed: 3000,
+  draggable: true,
+  pauseOnHover: false,
+  infinite: true,
+  dots: false,
+  arrows: true,
+  speed: 1000,
+
+  mobileFirst: true,
+
+  slidesToShow: 1,
+  slidesToScroll: 1,
+
+  responsive: [{
+      breakpoint: breakpoint.sm,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: breakpoint.md,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: breakpoint.lg,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3
+      }
+    },
+    {
+      breakpoint: breakpoint.xl,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3
+      }
     }
-
-		// Initialize all div with carousel class
-    var carousels = bulmaCarousel.attach('.carousel', options);
-	
-    bulmaSlider.attach();
-
-})
+  ]
+});
+});
